ded0b3896dd27477331b912f47429204b04fa3bc
mackeriv <mackeriv@gmail.com>
2023-07-13 15:11:43 -0300

    Moving things around to improve results when printing the grid.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,47 +12,40 @@ public class Main {
 
         String[][] gridArray = {{"A1", "A2", "A3"}, {"B1", "B2", "B3"}, {"C1", "C2", "C3"}};
 
-        System.out.println("Please choose a position from the grid: \n");
-
         String grid = ("""
                 A1 A2 A3
                 B1 B2 B3
                 C1 C2 C3""");
 
-        System.out.println(grid);
-
-        String userChoice = in.nextLine();
-
-        int dim1Pos = random.nextInt(gridArray.length);
-        int dim2Pos = random.nextInt(gridArray[dim1Pos].length);
-        String cpuChoice = gridArray[dim1Pos][dim2Pos];
-
-        //System.out.println("Please make your next choice: ");
-
         while (true) {
+
+            System.out.println("Please choose a position from the grid: \n");
+            System.out.println(grid);
+            String userChoice = in.nextLine();
+
+            int dim1Pos = random.nextInt(gridArray.length);
+            int dim2Pos = random.nextInt(gridArray[dim1Pos].length);
+            String cpuChoice = gridArray[dim1Pos][dim2Pos];
+
             for (int i = 0; i < gridArray.length; i++){
                 for (int j = 0; j < gridArray[i].length; j++){
 
                     if (userChoice.equals(gridArray[i][j])){
 
-                        System.out.println(grid.replace(gridArray[i][j],
-                                "\u001b[34;1m" + "\u001B[1m" + gridArray[i][j] + "\u001B[0m"));
-
+                        grid = grid.replace(gridArray[i][j],
+                                "\u001b[34;1m" + "\u001B[1m" + gridArray[i][j] + "\u001B[0m");
                     }
 
                     else if (cpuChoice.equals(gridArray[i][j])){
 
-                        System.out.println(grid.replace(gridArray[i][j],
-                                "\u001b[31;1m" + "\u001B[1m" + gridArray[i][j] + "\u001B[0m"));
-
+                        grid = grid.replace(gridArray[i][j],
+                                "\u001b[31;1m" + "\u001B[1m" + gridArray[i][j] + "\u001B[0m");
                     }
 
                 }
 
             }
-
-            break;
-
+            //break;
         }
     }
 }
