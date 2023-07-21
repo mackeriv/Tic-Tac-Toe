2923bf9957fc708594c7936e327735c19d366f73
mackeriv <mackeriv@gmail.com>
2023-07-21 14:42:39 -0300

    Added various comments, removed redundant flag.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,8 @@ public class Main {
         Scanner in = new Scanner(System.in);
         Random random = new Random();
         boolean found = false;
-        boolean second = false;
 
+        //creates and array of choices and a grid String to be printed for every turn
         String[][] gridArray = {{"A1", "A2", "A3"}, {"B1", "B2", "B3"}, {"C1", "C2", "C3"}};
 
         String grid = ("""
@@ -23,23 +23,28 @@ public class Main {
 
         while (true) {
 
-            if (second && found) {
+            //when everything goes right, prints a new statement after the first choices are made
+            if (found) {
                 System.out.println("Please make a new choice: \n");
             }
 
             System.out.println(grid);
             String userChoice = in.nextLine();
 
+            //quits the application
             if (userChoice.equalsIgnoreCase("Q")) {
                 break;
             }
 
+            //generates a random choice from the grid for the CPU
             int dim1Pos = random.nextInt(gridArray.length);
             int dim2Pos = random.nextInt(gridArray[dim1Pos].length);
             String cpuChoice = gridArray[dim1Pos][dim2Pos];
 
+            //resets the "found" flag the helps the program prompt the user for a valid choice
             found = false;
 
+            //loop that takes user input, finds it on the grid and amends the String with color formatting
             for (int i = 0; i < gridArray.length; i++) {
                 for (int j = 0; j < gridArray[i].length; j++) {
 
@@ -47,20 +52,24 @@ public class Main {
 
                         grid = grid.replace(gridArray[i][j],
                                 "\u001b[34;1m" + "\u001B[1m" + gridArray[i][j] + "\u001B[0m");
+                        //changes the chosen position on the grid String for "taken" and the "found" flag to true
                         gridArray[i][j] = "taken";
                         found = true;
                     }
                 }
             }
 
+            //loop that takes the randomized CPU choice, finds it on the grid and amends the String with color formatting
             outerLoop:
             for (int i = 0; i < gridArray.length; i++) {
                 for (int j = 0; j < gridArray[i].length; j++) {
 
+                    //if "found" flag is still set to "false", breaks loop and displays an error message
                     if (!found) {
                         break outerLoop;
                     }
 
+                    //if the CPU choice is taken, rolls a new random choice until the position is available
                     while (cpuChoice.equals("taken")) {
 
                         dim1Pos = random.nextInt(gridArray.length);
@@ -68,6 +77,7 @@ public class Main {
                         cpuChoice = gridArray[dim1Pos][dim2Pos];
                     }
 
+                    //finds the CPU choice and colors it, marks it as "taken" in the array
                     if (cpuChoice.equals(gridArray[i][j])) {
 
                         grid = grid.replace(gridArray[i][j],
@@ -78,11 +88,11 @@ public class Main {
                 }
             }
 
+            //error message to be displayed in case of invalid choices
             if (!found) {
                     System.out.println("Invalid option. Please pick a choice from the grid: \n");
             }
 
-            second = true;
         }
 
         System.out.println("Thank you for playing!");
